4e765408d898ab7dde3e5c28e61bf2c6fe814af5
Daniel160407 <abulashvilidani@hotmail.com>
2024-03-23 15:19:46 +0400

    Script moved to html file

--- src/navbarManager.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $(".nav-link").on("click", function() {
-      $(".nav-link").removeClass("active");
-      $(this).addClass("active");
-      var target = $(this).attr("href");
-      $(".tab-pane").removeClass("show active");
-      $(target).addClass("show active").fadeIn(600);
-    });
-  });
